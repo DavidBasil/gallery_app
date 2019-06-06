ce96a192174dc07b3f191cffe028b50ec9d190eb
David Basil <info@basildavid.com>
2019-06-06 12:09:17 +0400

    initial commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Image;
+use App\Album;
+
+class ImageController extends Controller
+{
+	public function index(){
+		return view('')
+	}
+}
